35da1a8aa4128ef0b003ffbfaa48932fbbe1cceb
Gavin Blair <gavin.blair@rtraction.com>
2014-07-26 20:41:00 -0400

    prevent scrolling for mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+document.body.addEventListener('touchmove', function(event) {
+  console.log(event.source);
+  //if (event.source == document.body)
+    event.preventDefault();
+}, false);
+
+window.onresize = function() {
+  $(document.body).width(window.innerWidth).height(window.innerHeight);
+}
+
+$(function() {
+  window.onresize();
+});
+
 $(document).ready(function(){
 	setInterval(function(){
 		$('#train').attr('data-left', parseInt($('#train').attr('data-left'),10)+100);
