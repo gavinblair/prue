bfbd7df90bb64beb69ad048176e3d453653416e5
Gavin Blair <gavin.blair@rtraction.com>
2014-07-26 20:37:43 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	setInterval(function(){
+		$('#train').attr('data-left', parseInt($('#train').attr('data-left'),10)+100);
+		$('#train').css('left', $('#train').attr('data-left')+"px");
+	}, 2000);
+
+	$(document).on('keydown click touchend', function(){
+		$('#prue').addClass('running');
+		$('#prue').attr('data-left', parseInt($('#prue').attr('data-left'),10)+10);
+		$('#prue').css('left', $('#prue').attr('data-left')+"px");
+		setInterval(function(){
+			$('#prue').removeClass('running');
+		}, 1000);
+	});
+});
